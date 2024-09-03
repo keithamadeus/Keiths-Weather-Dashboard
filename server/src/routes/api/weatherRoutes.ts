ab55eb6edf0f03a4e73455e39f5185fe6ba8f2ea
Keith <keith_a_w@hotmail.com>
2024-09-03 12:30:49 -0600

    add logic to htmlRoutes.ts and weatherRoutes.ts, still getting errors, but will look into what the issue is

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,60 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
   // TODO: GET weather data from city name
-  // TODO: save city to search history
+  try {
+    const weatherService = new WeatherService(
+      process.env.WEATHER_API_KEY || '',
+      req.body.cityName
+    );
+
+    // Fetch weather data
+    const weatherData = await weatherService.fetchWeatherData();
+
+    // save city to search history
+    const historyService = new HistoryService(
+      process.env.SEARCH_HISTORY_FILE || ''
+    );
+    await historyService.saveCity(weatherData);
+
+    // Send weather data as response
+    res.json(weatherData);
+  } catch (error) {
+    res.status(500).json({ message: (error as any).message });
+  }
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (_req: Request, res: Response) => {
+  try {
+
+    const historyService = new HistoryService(
+      process.env.SEARCH_HISTORY_FILE || ''
+    );
+    const history = await historyService.readHistory();
+    res.json(history);
+  } catch (error) {
+    res.status(500).json({ message: (error as any).message });
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  try {
+
+    const historyService = new HistoryService(
+      process.env.SEARCH_HISTORY_FILE || ''
+    );
+    await historyService.removeCity(req.params.id);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: (error as any).message });
+  }
+});
 
 export default router;
